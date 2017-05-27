ddaa8df7d25b6d1db4029b94ee990114c738e9b7
littleflute <13699175164@sina.cn>
2017-05-27 13:50:01 -0500

    Add files via upload

---
+++ Chimes of Freedom- The Songs of Bob Dylan Disc 2/m.js
@@ -0,0 +1,7 @@
+var f = [];				
+f[0]	= "v0.0.7";			
+f[1]	= "01 Outlaw Blues [_].mp3";		
+f[2]	= "02 Rainy Day Woman _12 & 35 [_].mp3";		
+f[3]	= "03 One More Cup of Coffee (Valley Below) [_].mp3";		
+f[4]	= "04 Heart of Mine [_].mp3";		
+f[5]	= "17 Make You Feel My Love [_][Live][Recorded Live at WXPN].mp3";		
